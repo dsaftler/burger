5863caeb759a89c8f05769d1074acc2e98ddbe1c
dsaftler <dsaftler@att.net>
2019-06-18 14:30:55 -0400

    Process.env for JAWSDB in connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection =mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "user",
   password: "MockTurb0Stu#ing",
   database: "burgers_db"
-});
-
+  });
+};
 connection.connect(function(err){
   if (err) {
     console.log("error connecting: "+err.stack);
